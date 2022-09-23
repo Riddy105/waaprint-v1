8a74d91a0c1be6015687f6a033a24f56ed5968de
Riddy105 <olaniranolatubosun@gmail.com>
2022-09-23 15:21:46 +0100

    Started work on project

---
+++ README.md
@@ -0,0 +1,1 @@
+"Beginning of waaprint version 1" 
---
+++ script.js
@@ -0,0 +1,2 @@
+console.log(`Trying git pull out and it's still confusing`);
+// Now my first commit reflected locally on my machine, so I'm gonna write a bit more,
