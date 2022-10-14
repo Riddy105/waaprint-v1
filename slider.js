d866d9c1aa2960c73cda33f8894e644605d36e1a
Riddy105 <olaniranolatubosun@gmail.com>
2022-10-14 12:37:37 +0100

    Fixing Fade-in

--- slider.js
+++ slider.js
@@ -13,15 +13,17 @@ setInterval(tick, 2500);
 
 // IMPLEMENTING QUALITY SECTION FADE-IN WITH INTERSECTION OBSERVER
 const qualitySectionEl = document.querySelector(`.quality-section`);
+qualitySectionEl.classList.add(`fade-in`); // For performance, I am hiding the section with javascript so it's completely visible to anyone who has turned off JS engine
+// const navElement = document.querySelector(`nav`);
 const ObsCallBack = function (entries) {
   const [entry] = entries;
   if (entry.isIntersecting) {
-    qualitySectionEl.classList.add(`fade-in`);
-  } else {
     qualitySectionEl.classList.remove(`fade-in`);
+    // navElement.classList.remove(`nav-open`);
+  } else {
+    qualitySectionEl.classList.add(`fade-in`);
   }
 };
-
 const observer = new IntersectionObserver(ObsCallBack, {
   root: null,
   threshold: 0,
